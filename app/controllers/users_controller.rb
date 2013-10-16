52c1f1c6d82016973ba26beb6ca47aae6aa88fd2
JoseDiego <jdiazc6@gmail.com>
2013-10-16 05:17:13 -0500

    Terminando CRUD para Usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,29 @@
 class UsersController < ApplicationController
-
-   def show
-    @user = User.find(params[:id])
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
+  def index
+     @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Cambios Guardados"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def new
     @user = User.new
   end
@@ -28,5 +44,19 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Porfavor Inicie Sesion."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
